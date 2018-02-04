4ad361075c847328b07d993e40424a9e65e65e5c
Mariano UG <marianok300@gmail.com>
2018-02-03 19:10:10 -0600

    Added List rendering for Markers

--- assets/containers/Map.js
+++ assets/containers/Map.js
@@ -38,58 +38,64 @@ export default class App extends React.Component {
 
   onCollectionUpdate = (query) => {
     query.forEach((doc) => {
-      console.log(doc.pos);
-      /*const id = doc.id;
+      //console.log(doc.data().pos['_lat']);
       this.setState({ cam: { ...this.state.cam, 
-        [id]: { 
-          id: doc.id,
-          lat: [doc.pos.getLatitude()],
-          long: [doc.pos.getLongitude()],
+        [doc.data().id]: { 
+          id: doc.data().id,
+          lat: doc.data().pos['_lat'],
+          long: doc.data().pos['_long'],
       } }
-    });*/
+    });
     });
 
-    console.log(this.state.cam);
+    this.setState({ carsLoaded: true });
+    //console.log(this.state.cam);
   }
+
+  renderCars() {
+   // if (Object.keys(this.state.cam).length > 0) {
+
+    const arr = Object.keys(this.state.cam).map((key) => { return this.state.cam[key]; });
+    if (arr.length > 0) {
+      return arr.map((item) => {
+        return (
+          <MapView.Marker
+            coordinate={{ latitude: item.lat,
+            longitude: item.long, }}
+          >
+            <Image ref='image' style={styles.markerStyle} source={car} />
+          </MapView.Marker>
+        );
+      });
+    }
+    return null;
+  }
+    
 
   render() {
     Keyboard.dismiss();
-    console.log("Hola");
-    const style = {
-      width: 34,
-      height: 14,
-      transform: [
-        { rotate: '0deg' }
-      ]
-    };
-
     return (
-      <Container>
-        <StatusBar
-          barStyle="dark-content"
-        />
-        <MapView
-        rotateEnabled={false}
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 25.649173,
-          longitude: -100.289758,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        >
-       <MapView.Marker
-        coordinate={{ latitude: 25.649173,
-          longitude: -100.289758, }}
-        rotation={120.0}
-       >
-       <Image ref='image' style={style} source={car} />
-       </MapView.Marker>
-       </MapView>
-       {
-         this.state.fontLoaded ? <AnimatedDrawer /> : null
-       }
-      </Container>
+          this.state.carsLoaded ? (
+            <Container>
+              <StatusBar
+                barStyle="dark-content"
+              />
+              <MapView
+              loadingEnabled
+              rotateEnabled={false}
+              style={{ flex: 1 }}
+              initialRegion={{
+                latitude: 25.649173,
+                longitude: -100.289758,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+              >
+                {this.renderCars()}
+              </MapView>
+                {this.state.fontLoaded ? <AnimatedDrawer /> : null}
+             </Container>
+          ) : null
     );
   }
 }
@@ -105,5 +111,12 @@ const styles = StyleSheet.create({
     marginRight: '4%',
     marginTop: '2%',
   },
+  markerStyle: {
+    width: 34,
+    height: 14,
+    transform: [
+      { rotate: '0deg' }
+    ]
+  }
 
 });
